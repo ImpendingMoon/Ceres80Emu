89286735fd6e23d04b1a167d605e770cdd68afec
ImpendingMoon <theimpendingmoon@gmail.com>
2025-03-30 15:16:36 -0500

    Create Emulator Classes

---
+++ Emulator/Ceres80.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceres80Emu.Emulator
+{
+    internal class Ceres80
+    {
+
+    }
+}
