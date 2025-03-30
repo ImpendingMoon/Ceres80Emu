f1d8e35c0a531cfa287029e892083815f6a54c5b
ImpendingMoon <theimpendingmoon@gmail.com>
2025-03-30 16:29:38 -0500

    Start implementing Z80 class
    - Made Z80 class partial
    - Created Z80Execute.cs to separate decoding and execution from main user class
    - Created Run, Step, and Reset methods

--- Emulator/Z80.cs
+++ Emulator/Z80.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace Ceres80Emu.Emulator
 {
-    internal class Z80
+    /// <summary>
+    /// Emulates a Z80 CPU.
+    /// Z80.cs handles most methods and properties, while Z80Execute.cs Z80 handles execution.
+    /// This is because decoding takes several thousand lines of code.
+    /// </summary>
+    internal partial class Z80
     {
+        public Z80(MemoryBus memoryBus, InterruptManager interruptManager)
+        {
+            _memoryBus = memoryBus;
+            _interruptManager = interruptManager;
+            _registers = new RegisterSet();
+        }
+
+        public Z80(MemoryBus memoryBus, InterruptManager interruptManager, RegisterSet registers)
+        {
+            _memoryBus = memoryBus;
+            _interruptManager = interruptManager;
+            _registers = registers;
+        }
+
+
+
+        /// <summary>
+        /// Executes a certain number of machine cycles (T-states)
+        /// </summary>
+        /// <param name="steps">Minimum number of cycles to execute</param>
+        /// <returns>Number of cycles executed</returns>
+        public int Run(int steps)
+        {
+            int cycles = 0;
+
+            while (cycles < steps)
+            {
+                cycles += Step();
+            }
+
+            return cycles;
+        }
+
+
+
+        /// <summary>
+        /// Resets the CPU to its initial state.
+        /// </summary>
+        public void Reset()
+        {
+            // PC starts at 0x0000, others technically undefined, use 0 for simplicity
+            foreach (var field in _registers.GetType().GetFields())
+            {
+                field.SetValue(_registers, 0);
+            }
+            // Stack pointer starts at top of RAM
+            _registers.SP = 0xFFFF;
+        }
+
+
+
+        private MemoryBus _memoryBus;
+        private InterruptManager _interruptManager;
+        private RegisterSet _registers;
     }
 }
---
+++ Emulator/Z80Execute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceres80Emu.Emulator
+{
+    internal partial class Z80
+    {
+        /// <summary>
+        /// Executes a single instruction
+        /// </summary>
+        /// <returns>Number of cycles taken</returns>
+        public int Step()
+        {
+            return 0;
+        }
+    }
+}
