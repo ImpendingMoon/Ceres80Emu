852ce42bebc64528f0623eb02597516c6f25e7d1
ImpendingMoon <theimpendingmoon@gmail.com>
2025-03-30 20:19:20 -0500

    Add arithmetic and logical instructions
    - Fix timing on block transfer (forgot to remove opcode fetch time)
    - Make section comments more obvious
    - Added more untested instructions

--- Emulator/Z80Execute.cs
+++ Emulator/Z80Execute.cs
@@ -10,7 +10,7 @@ namespace Ceres80Emu.Emulator
     // All methods return the number of cycles taken
     internal partial class  Z80
     {
-        // Load and Exchange
+        /************************* Load and Exchange *************************/
 
         /// <summary>
         /// Loads a register into another register
@@ -207,7 +207,9 @@ namespace Ceres80Emu.Emulator
             return 10;
         }
 
-        // Block Transfer and Search
+
+
+        /********************* Block Transfer and Search *********************/
 
         /// <summary>
         /// Loads a value in memory from (HL) to (DE), incrementing both HL and DE and decrementing BC
@@ -225,7 +227,7 @@ namespace Ceres80Emu.Emulator
             _registers.HalfCarry = false;
             _registers.ParityOverflow = _registers.BC != 0;
 
-            return 16;
+            return 12;
         }
 
         /// <summary>
@@ -244,7 +246,7 @@ namespace Ceres80Emu.Emulator
             _registers.HalfCarry = false;
             _registers.ParityOverflow = _registers.BC != 0;
 
-            return 16;
+            return 12;
         }
 
         /// <summary>
@@ -264,7 +266,7 @@ namespace Ceres80Emu.Emulator
             _registers.Zero = result == 0;
             _registers.Sign = (result & 0x80) != 0;
 
-            return 16;
+            return 12;
         }
 
         /// <summary>
@@ -284,18 +286,413 @@ namespace Ceres80Emu.Emulator
             _registers.Zero = result == 0;
             _registers.Sign = (result & 0x80) != 0;
 
-            return 16;
+            return 12;
         }
 
-        // Arithmetic and Logical
 
-        // Rotate and Shift
 
-        // Bit Manipulation
+        /********************** Arithmetic and Logical ***********************/
 
-        // Jump, Call, and Return
+        /// <summary>
+        /// Adds a register to another register
+        /// <br/>Example: ADD A, B
+        /// </summary>
+        private int Add_Reg_Reg(ref byte dest, byte src, bool carry = false)
+        {
+            dest = Add8(dest, src, carry);
+            return 4;
+        }
 
-        // Input/Output
+        /// <summary>
+        /// Adds an immediate value to a register
+        /// <br/>Example: ADD A, n
+        /// </summary>
+        private int Add_Reg_Imm(ref byte dest, bool carry = false)
+        {
+            dest = Add8(dest, ReadImm(), carry);
+            return 7;
+        }
+
+        /// <summary>
+        /// Adds the value at a memory location pointed to by a register pair to a register
+        /// <br/>Example: ADD A, (HL)
+        /// </summary>
+        private int Add_Reg_Reg16Ptr(ref byte dest, ushort src, bool carry = false)
+        {
+            dest = Add8(dest, _memoryBus.Read(src), carry);
+            return 7;
+        }
+
+        /// <summary>
+        /// Adds the value at a memory location pointed to by an index + offset to a register
+        /// <br/>Example: ADD A, (IX+d)
+        /// </summary>
+        private int Add_Reg_Index(ref byte dest, ushort index, bool carry = false)
+        {
+            byte offset = ReadImm();
+            ushort address = (ushort)(index + offset);
+            dest = Add8(dest, _memoryBus.Read(address), carry);
+            return 15;
+        }
+
+        /// <summary>
+        /// Adds a register pair to another register pair
+        /// <br/>Example: ADD HL, BC
+        /// </summary>
+        private int Add_Reg16_Reg16(ref ushort dest, ushort src, bool carry = false)
+        {
+            bool overflow = WillOverflow<ushort>(dest, src);
+            dest += (ushort)(src + (carry ? 1 : 0));
+
+            _registers.Carry = overflow;
+            _registers.Subtract = false;
+            _registers.HalfCarry = (dest & 0x0FFF) < (src & 0x0FFF);
+
+            // ADC rr, rr' has different behavior than ADD rr, rr'
+            if(carry)
+            {
+                _registers.ParityOverflow = overflow;
+                _registers.Zero = dest == 0;
+                _registers.Sign = (dest & 0x8000) != 0;
+            }
+
+            return 11;
+        }
+
+        /// <summary>
+        /// Increments a register
+        /// <br/>Example: INC A
+        /// </summary>
+        private int Inc_Reg(ref byte reg)
+        {
+            reg = Add8(reg, 1, false, false);
+            return 4;
+        }
+
+        /// <summary>
+        /// Increments the value at a memory location pointed to by a register pair
+        /// <br/>Example: INC (HL)
+        /// </summary>
+        private int Inc_Reg16Ptr(ushort address)
+        {
+            byte value = _memoryBus.Read(address);
+            _memoryBus.Write(address, Add8(value, 1, false, false));
+            return 11;
+        }
+
+        /// <summary>
+        /// Increments a register pair
+        /// <br/>Example: INC BC
+        /// </summary>
+        private int Inc_Reg16(ref ushort reg)
+        {
+            reg++;
+            return 6;
+        }
+
+        /// <summary>
+        /// Increments the value at a memory location pointed to by an index + offset
+        /// <br/>Example: INC (IX+d)
+        /// </summary>
+        private int Inc_Index(ushort index)
+        {
+            byte offset = ReadImm();
+            ushort address = (ushort)(index + offset);
+            byte value = _memoryBus.Read(address);
+            _memoryBus.Write(address, Add8(value, 1, false, false));
+
+            return 19;
+        }
+
+        /// <summary>
+        /// Subtracts a register from another register
+        /// <br/>Example: SUB A, B
+        /// </summary>
+        private int Sub_Reg_Reg(ref byte dest, byte src, bool carry = false)
+        {
+            dest = Sub8(dest, src, carry);
+            return 4;
+        }
+
+        /// <summary>
+        /// Subtracts an immediate value from a register
+        /// <br/>Example: SUB A, n
+        /// </summary>
+        private int Sub_Reg_Imm(ref byte dest, bool carry = false)
+        {
+            dest = Sub8(dest, ReadImm(), carry);
+            return 7;
+        }
+
+        /// <summary>
+        /// Subtracts the value at a memory location pointed to by a register pair from a register
+        /// <br/>Example: SUB A, (HL)
+        /// </summary>
+        private int Sub_Reg_Reg16Ptr(ref byte dest, ushort src, bool carry = false)
+        {
+            dest = Sub8(dest, _memoryBus.Read(src), carry);
+            return 7;
+        }
+
+        /// <summary>
+        /// Subtracts the value at a memory location pointed to by an index + offset from a register
+        /// <br/>Example: SUB A, (IX+d)
+        /// </summary>
+        private int Sub_Reg_Index(ref byte dest, ushort index, bool carry = false)
+        {
+            byte offset = ReadImm();
+            ushort address = (ushort)(index + offset);
+            dest = Sub8(dest, _memoryBus.Read(address), carry);
+            return 15;
+        }
+
+        /// <summary>
+        /// Subtracts a register pair from another register pair
+        /// <br/>Example: SUB HL, BC
+        /// </summary>
+        private int Sub_Reg16_Reg16(ref ushort dest, ushort src)
+        {
+            bool underflow = WillUnderflow<ushort>(dest, src);
+            dest -= (ushort)(src - 1);
+
+            _registers.Carry = underflow;
+            _registers.Subtract = true;
+            _registers.ParityOverflow = underflow;
+            _registers.HalfCarry = (dest & 0x0FFF) > (src & 0x0FFF);
+            _registers.Zero = dest == 0;
+            _registers.Sign = (dest & 0x8000) != 0;
+
+            return 11;
+        }
+
+        /// <summary>
+        /// Decrements a register
+        /// <br/>Example: DEC A
+        /// </summary>
+        private int Dec_Reg(ref byte reg)
+        {
+            reg = Sub8(reg, 1, false, false);
+            return 4;
+        }
+
+        /// <summary>
+        /// Decrements the value at a memory location pointed to by a register pair
+        /// <br/>Example: DEC (HL)
+        /// </summary>
+        private int Dec_Reg16Ptr(ushort addressess)
+        {
+            byte value = _memoryBus.Read(addressess);
+            _memoryBus.Write(addressess, Sub8(value, 1, false, false));
+            return 11;
+        }
+
+        /// <summary>
+        /// Decrements a register pair
+        /// <br/>Example: DEC BC
+        /// </summary>
+        private int Dec_Reg16(ref ushort reg)
+        {
+            reg--;
+            return 6;
+        }
+
+        /// <summary>
+        /// Decrements the value at a memory location pointed to by an index + offset
+        /// <br/>Example: DEC (IX+d)
+        /// </summary>
+        private int Dec_Index(ushort index)
+        {
+            byte offset = ReadImm();
+            ushort address = (ushort)(index + offset);
+            byte value = _memoryBus.Read(address);
+            _memoryBus.Write(address, Sub8(value, 1, false, false));
+
+            return 19;
+        }
+
+        /// <summary>
+        /// Compares a register with another register
+        /// <br/>Example: CP A, B
+        /// </summary>
+        private int Compare_Reg_Reg(byte dest, byte src)
+        {
+            Sub8(dest, src, false);
+            return 4;
+        }
+
+        /// <summary>
+        /// Compares a register with an immediate value
+        /// <br/>Example: CP A, n
+        /// </summary>
+        private int Compare_Reg_Imm(byte dest)
+        {
+            Sub8(dest, ReadImm(), false);
+            return 7;
+        }
+
+        /// <summary>
+        /// Compares a register with the value at a memory location pointed to by a register pair
+        /// <br/>Example: CP A, (HL)
+        /// </summary>
+        private int Compare_Reg_Reg16Ptr(byte dest, ushort src)
+        {
+            Sub8(dest, _memoryBus.Read(src), false);
+            return 7;
+        }
+
+        /// <summary>
+        /// Compares a register with the value at a memory location pointed to by an index + offset
+        /// <br/>Example: CP A, (IX+d)
+        /// </summary>
+        private int Compare_Reg_Index(ref byte dest, ushort index)
+        {
+            byte offset = ReadImm();
+            ushort address = (ushort)(index + offset);
+            Sub8(dest, _memoryBus.Read(address), false);
+            return 15;
+        }
+
+        /// <summary>
+        /// Performs a bitwise AND operation on a register and another register
+        /// <br/>Example: AND A, B
+        /// </summary>
+        private int And_Reg_Reg(ref byte dest, byte src)
+        {
+            dest = And8(dest, src);
+            return 4;
+        }
+
+        /// <summary>
+        /// Performs a bitwise AND operation on a register and an immediate value
+        /// <br/>Example: AND A, n
+        /// </summary>
+        private int And_Reg_Imm(ref byte dest, bool carry = false)
+        {
+            dest = And8(dest, ReadImm());
+            return 7;
+        }
+
+        /// <summary>
+        /// Performs a bitwise AND operation on a register and the value at a memory location pointed to by a register pair
+        /// <br/>Example: AND A, (HL)
+        /// </summary>
+        private int And_Reg_Reg16Ptr(ref byte dest, ushort src)
+        {
+            dest = And8(dest, _memoryBus.Read(src));
+            return 7;
+        }
+
+        /// <summary>
+        /// Performs a bitwise AND operation on a register and the value at a memory location pointed to by an index + offset
+        /// <br/>Example: AND A, (IX+d)
+        /// </summary>
+        private int And_Reg_Index(ref byte dest, ushort index)
+        {
+            byte offset = ReadImm();
+            ushort address = (ushort)(index + offset);
+            dest = And8(dest, _memoryBus.Read(address));
+            return 15;
+        }
+
+        /// <summary>
+        /// Performs a bitwise OR operation on a register and another register
+        /// <br/>Example: OR A, B
+        /// </summary>
+        private int Or_Reg_Reg(ref byte dest, byte src)
+        {
+            dest = Or8(dest, src);
+            return 4;
+        }
+
+        /// <summary>
+        /// Performs a bitwise OR operation on a register and an immediate value
+        /// <br/>Example: OR A, n
+        /// </summary>
+        private int Or_Reg_Imm(ref byte dest, bool carry = false)
+        {
+            dest = Or8(dest, ReadImm());
+            return 7;
+        }
+
+        /// <summary>
+        /// Performs a bitwise OR operation on a register and the value at a memory location pointed to by a register pair
+        /// <br/>Example: OR A, (HL)
+        /// </summary>
+        private int Or_Reg_Reg16Ptr(ref byte dest, ushort src)
+        {
+            dest = Or8(dest, _memoryBus.Read(src));
+            return 7;
+        }
+
+        /// <summary>
+        /// Performs a bitwise OR operation on a register and the value at a memory location pointed to by an index + offset
+        /// <br/>Example: OR A, (IX+d)
+        /// </summary>
+        private int Or_Reg_Index(ref byte dest, ushort index)
+        {
+            byte offset = ReadImm();
+            ushort address = (ushort)(index + offset);
+            dest = Or8(dest, _memoryBus.Read(address));
+            return 15;
+        }
+
+        /// <summary>
+        /// Performs a bitwise XOR operation on a register and another register
+        /// <br/>Example: XOR A, B
+        /// </summary>
+        private int Xor_Reg_Reg(ref byte dest, byte src)
+        {
+            dest = Xor8(dest, src);
+            return 4;
+        }
+
+        /// <summary>
+        /// Performs a bitwise XOR operation on a register and an immediate value
+        /// <br/>Example: XOR A, n
+        /// </summary>
+        private int Xor_Reg_Imm(ref byte dest, bool carry = false)
+        {
+            dest = Xor8(dest, ReadImm());
+            return 7;
+        }
+
+        /// <summary>
+        /// Performs a bitwise XOR operation on a register and the value at a memory location pointed to by a register pair
+        /// <br/>Example: XOR A, (HL)
+        /// </summary>
+        private int Xor_Reg_Reg16Ptr(ref byte dest, ushort src)
+        {
+            dest = Xor8(dest, _memoryBus.Read(src));
+            return 7;
+        }
+
+        /// <summary>
+        /// Performs a bitwise XOR operation on a register and the value at a memory location pointed to by an index + offset
+        /// <br/>Example: XOR A, (IX+d)
+        /// </summary>
+        private int Xor_Reg_Index(ref byte dest, ushort index)
+        {
+            byte offset = ReadImm();
+            ushort address = (ushort)(index + offset);
+            dest = Xor8(dest, _memoryBus.Read(address));
+            return 15;
+        }
+
+
+
+        /************************* Rotate and Shift **************************/
+
+
+
+        /************************* Bit Manipulation **************************/
+
+
+
+        /********************** Jump, Call, and Return ***********************/
+
+
+
+        /*************************** Input/Output ****************************/
 
         /// <summary>
         /// Loads a value from Port (BC) to (HL), incrementing HL and decrementing B
@@ -312,7 +709,7 @@ namespace Ceres80Emu.Emulator
             _registers.Subtract = true;
             _registers.Zero = _registers.BC == 0; // ??? (Need to find documentation)
 
-            return 16;
+            return 12;
         }
 
         /// <summary>
@@ -330,7 +727,7 @@ namespace Ceres80Emu.Emulator
             _registers.Subtract = true;
             _registers.Zero = _registers.BC == 0; // ??? (Need to find documentation)
 
-            return 16;
+            return 12;
         }
 
         /// <summary>
@@ -348,7 +745,7 @@ namespace Ceres80Emu.Emulator
             _registers.Subtract = true;
             _registers.Zero = _registers.BC == 0; // ??? (Need to find documentation)
 
-            return 16;
+            return 12;
         }
 
         /// <summary>
@@ -366,10 +763,12 @@ namespace Ceres80Emu.Emulator
             _registers.Subtract = true;
             _registers.Zero = _registers.BC == 0; // ??? (Need to find documentation)
 
-            return 16;
+            return 12;
         }
 
-        // CPU Control
+
+
+        /**************************** CPU Control ****************************/
 
         /// <summary>
         /// Does Nothing
@@ -429,5 +828,84 @@ namespace Ceres80Emu.Emulator
         {
             return a.CompareTo(default(T)) < 0 && b.CompareTo(default(T)) < 0 && a.CompareTo(default(T)) + b.CompareTo(default(T)) > 0;
         }
-    }
+
+        private byte Add8(byte a, byte b, bool carry, bool setCarry = true)
+        {
+            byte result = (byte)(a + b + (carry ? 1 : 0));
+            bool overflow = WillOverflow<byte>(a, (byte)(b + (carry ? 1 : 0)));
+
+            // Increment instructions don't set carry. For some reason.
+            if (setCarry)
+                _registers.Carry = overflow;
+            _registers.Subtract = false;
+            _registers.ParityOverflow = overflow;
+            _registers.HalfCarry = (a & 0x0F) < (b & 0x0F);
+            _registers.Zero = result == 0;
+            _registers.Sign = (result & 0x80) != 0;
+
+            return result;
+        }
+
+        private byte Sub8(byte a, byte b, bool carry, bool setCarry = true)
+        {
+            byte result = (byte)(a - b - (carry ? 1 : 0));
+            bool underflow = WillUnderflow<byte>(a, (byte)(b + (carry ? 1 : 0)));
+
+            if (setCarry)
+                _registers.Carry = underflow;
+            _registers.Subtract = true;
+            _registers.ParityOverflow = underflow;
+            _registers.HalfCarry = (a & 0x0F) > (b & 0x0F);
+            _registers.Zero = result == 0;
+            _registers.Sign = (result & 0x80) != 0;
+
+            return result;
+        }
+
+        private byte And8(byte a, byte b)
+        {
+            byte result = (byte)(a & b);
+            _registers.Carry = false;
+            _registers.Subtract = false;
+            _registers.ParityOverflow = Parity(result);
+            _registers.HalfCarry = true;
+            _registers.Zero = result == 0;
+            _registers.Sign = (result & 0x80) != 0;
+            return result;
+        }
+
+        private byte Or8(byte a, byte b)
+        {
+            byte result = (byte)(a | b);
+            _registers.Carry = false;
+            _registers.Subtract = false;
+            _registers.ParityOverflow = Parity(result);
+            _registers.HalfCarry = false;
+            _registers.Zero = result == 0;
+            _registers.Sign = (result & 0x80) != 0;
+            return result;
+        }
+
+        private byte Xor8(byte a, byte b)
+        {
+            byte result = (byte)(a ^ b);
+            _registers.Carry = false;
+            _registers.Subtract = false;
+            _registers.ParityOverflow = Parity(result);
+            _registers.HalfCarry = false;
+            _registers.Zero = result == 0;
+            _registers.Sign = (result & 0x80) != 0;
+            return result;
+        }
+
+        private bool Parity(byte a)
+        {
+            int count = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                if ((a & (1 << i)) != 0)
+                    count++;
+            }
+            return count % 2 == 0;
+        }
 }
