6c9e4fc1ff6f4ea93444aabc1120b951fd00031c
ImpendingMoon <theimpendingmoon@gmail.com>
2025-04-15 15:08:15 -0500

    Start implementing LCD
    - Implement LCD.cs
    - Create LCDSection.cd
    - Handle reading/writing to LCD
    - Added GetPixelData to convert from packed framebuffer data to correctly oriented pixel data
    - Added GetBitmap to convert pixel data from both LCD sections to a drawable bitmap

--- Emulator/LCD.cs
+++ Emulator/LCD.cs
@@ -1,32 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace Ceres80Emu.Emulator
 {
     internal class LCD: IMemoryDevice
     {
+        public LCD()
+        {
+            _lcdBitmap = new Bitmap(128, 64, PixelFormat.Format8bppIndexed);
+            _palette = _lcdBitmap.Palette;
+            _palette.Entries[0] = Color.Black;
+            _palette.Entries[1] = Color.White;
+            _lcdBitmap.Palette = _palette;
+        }
+
         public ushort Size { get; } = 4;
-        public bool CanWrite { get; } = true;
-        public bool CanRead { get; } = true;
 
         public byte Read(ushort address)
         {
-            return 0x00;
+            switch(address % Size)
+            {
+                case 0: return _left.ReadStatus();
+                case 1: return _left.ReadData();
+                case 2: return _right.ReadStatus();
+                case 3: return _right.ReadData();
+                default: return 0xFF;
+            }
         }
 
         public void Write(ushort address, byte data)
         {
+            switch(address % Size)
+            {
+                case 0: _left.WriteCommand(data); break;
+                case 1: _left.WriteData(data); break;
+                case 2: _right.WriteCommand(data); break;
+                case 3: _right.WriteData(data); break;
+            }
         }
 
         public void Reset()
         {
+            _left.Reset();
+            _right.Reset();
         }
 
         public void Tick()
         {
+        }
+
+        public Bitmap GetBitmap()
+        {
+            byte[,] leftPixels = _left.GetPixelData();
+            byte[,] rightPixels = _right.GetPixelData();
+
+            BitmapData data = _lcdBitmap.LockBits(
+                new Rectangle(0, 0, 128, 64),
+                ImageLockMode.WriteOnly,
+                PixelFormat.Format8bppIndexed
+            );
+
+            int stride = data.Stride;
+            int height = data.Height;
+            int width = data.Width;
+
+            // Combine two halves into one array
+            byte[] pixelBytes = new byte[stride * height];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int pixelIndex = y * stride + x;
+                    byte value = (x < 64) ? leftPixels[x, y] : rightPixels[x - 64, y];
+                    pixelBytes[pixelIndex] = value;
+                }
+            }
+
+            Marshal.Copy(pixelBytes, 0, data.Scan0, pixelBytes.Length);
+            _lcdBitmap.UnlockBits(data);
+
+            return _lcdBitmap;
         }
 
         public byte[] SaveState()
@@ -37,5 +91,11 @@ namespace Ceres80Emu.Emulator
         public void LoadState(byte[] state)
         {
         }
+
+        LCDSection _left = new LCDSection();
+        LCDSection _right = new LCDSection();
+
+        Bitmap _lcdBitmap = new Bitmap(128, 64, PixelFormat.Format8bppIndexed);
+        ColorPalette _palette;
     }
 }
---
+++ Emulator/LCDSection.cs
@@ -0,0 +1,97 @@
+﻿namespace Ceres80Emu.Emulator
+{
+    // https://support.newhavendisplay.com/hc/en-us/article_attachments/4414521335447
+    internal class LCDSection
+    {
+        public byte ReadData()
+        {
+            byte value = _framebuffer[_y, _x];
+            // Increment not documented on command overview, but is in details (13)
+            _y = (byte)((_y + 1) % 64);
+            return value;
+        }
+
+        public void WriteData(byte data)
+        {
+            _framebuffer[_y, _x] = data;
+            _y = (byte)((_y + 1) % 64);
+        }
+
+        public byte ReadStatus()
+        {
+            // Not emulating busy states or display corruption
+            return (byte)(_displayOn ? 0b00100000 : 0);
+        }
+
+        public void WriteCommand(byte data)
+        {
+            // Display On/Off
+            if ((data & 0b00111110) == 0b00111110)
+            {
+                _displayOn = (data & 1) == 1;
+            }
+            // Set Address (Y address)
+            else if ((data & 0b01000000) == 0b01000000)
+            {
+                _y = (byte)(data & 0b00111111);
+            }
+            // Set Page (X address)
+            else if ((data & 0b10100000) == 0b10100000)
+            {
+                _x = (byte)(data & 0b00000111);
+            }
+            // Display start line (Z address)
+            else if ((data & 0b11000000) == 0b11000000)
+            {
+                _z = (byte)(data & 0b00111111);
+            }
+        }
+
+        public void Reset()
+        {
+            _framebuffer = new byte[64, 8];
+            _y = 0;
+            _x = 0;
+            _z = 0;
+            _displayOn = false;
+        }
+
+        public byte[,] GetPixelData()
+        {
+            byte[,] data = new byte[64, 64];
+
+            if(_displayOn)
+            {
+                // Screen is laid out in a very weird way
+                // X pages are vertical, but X=0 is still at the top.
+                // LSB of pages are at the top.
+                // Y and Z are horizontal.
+                // There is no orientation where this makes sense, but that's how it is.
+
+                for(int srcY = 0; srcY < height; srcY++)
+                {
+                    for(int srcX = 0; srcX < width; srcX++)
+                    {
+                        for(int bit = 0; bit < 8; bit++)
+                        {
+                            int destX = (srcY + _z) % height; // Same X = 1 column
+                            int destY = srcX * 8 + bit; // Different Y = 8 rows
+                            data[destX, destY] = (byte)((_framebuffer[srcX, srcY] >> bit) & 1);
+                        }
+                    }
+                }
+            }
+
+            return data;
+        }
+
+        private byte[,] _framebuffer = new byte[64, 8];
+        private byte _y = 0; // Row (0-64)
+        private byte _x = 0; // 8-Pixel Column (0-8)
+        private byte _z = 0; // Vertical Scroll (0-64)
+        private bool _displayOn = false;
+
+        private const int width = 8;
+        private const int height = 64;
+    }
+}
