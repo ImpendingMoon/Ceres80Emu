aea5298e1637825a4bf46ced94677b92ed7b6317
ImpendingMoon <theimpendingmoon@gmail.com>
2025-04-14 19:34:39 -0500

    Update Ceres80.cs
    - Implement new interface
    - Use Stopwatch instead of Timer (now requires separate thread)
    - Remove SaveState() and LoadState()
    - Added LoadRAM()
    - Changed speed to use multiplier

--- Emulator/Ceres80.cs
+++ Emulator/Ceres80.cs
@@ -1,13 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace Ceres80Emu.Emulator
 {
     internal class Ceres80
     {
+        /// <summary>
+        /// Start the emulator.
+        /// Will lock up the UI thread if run on the main thread.
+        /// </summary>
+        public void Start()
+        {
+            _running = true;
+            while (_running)
+            {
+                RunFrame();
+            }
+        }
+
+        /// <summary>
+        /// Stop the emulator.
+        /// </summary>
+        public void Stop()
+        {
+            _running = false;
+        }
+
+        /// <summary>
+        /// Execute a single instruction.
+        /// </summary>
+        public void Step()
+        {
+            Tick(1);
+        }
+
+        /// <summary>
+        /// Set the speed of the emulator.
+        /// </summary>
+        /// <param name="multiplier">Percent of normal speed. 100x is default. 0x is single-step mode.</param>
+        /// <exception cref="ArgumentException">When multiplier is less than 0 or greater than 500</exception>
+        public void SetSpeed(int multiplier)
+        {
+            if (multiplier < 0 || multiplier > 500)
+            {
+                throw new ArgumentException("Speed must be between 0x and 500x");
+            }
+            _instructionsPerFrame = (int)(_instructionsPerSecond * (multiplier / 100f)) / _framesPerSecond;
+        }
+
+        /// <summary>
+        /// Load a ROM into the emulator.
+        /// </summary>
+        /// <param name="data">Data to load</param>
+        /// <exception cref="ArgumentException">If data size > 32768 bytes</exception>
+        public void LoadROM(byte[] data)
+        {
+            if (data.Length > _rom.Size)
+            {
+                throw new ArgumentException("File size exceeds ROM size.");
+            }
+            _rom.LoadState(data);
+        }
+
+        /// <summary>
+        /// Load a RAM image into the emulator.
+        /// </summary>
+        /// <param name="data">Data to load</param>
+        /// <exception cref="ArgumentException">If data size > 32768 bytes</exception>
+        public void LoadRAM(byte[] data)
+        {
+            if (data.Length > _ram.Size)
+            {
+                throw new ArgumentException("File size exceeds RAM size.");
+            }
+            _ram.LoadState(data);
+        }
+
         public Ceres80()
         {
             // Initialization order matters. Do not change.
@@ -19,116 +86,58 @@ namespace Ceres80Emu.Emulator
             _lcd = new LCD();
             _bus = new MemoryBus();
 
-            _bus.AddDevice(_rom, 0x0000, 0x7FFF, false);
-            _bus.AddDevice(_ram, 0x8000, 0xFFFF, false);
-            _bus.AddDevice(_ctc, 0x0000, 0x0003, true);
-            _bus.AddDevice(_pio, 0x0004, 0x0007, true);
-            _bus.AddDevice(_lcd, 0x0008, 0x000B, true);
+            _bus.AddMemoryDevice(_rom, 0x0000, 0x7FFF);
+            _bus.AddMemoryDevice(_ram, 0x8000, 0xFFFF);
+            _bus.AddPortDevice(_ctc, 0x0000, 0x0003);
+            _bus.AddPortDevice(_pio, 0x0004, 0x0007);
+            _bus.AddPortDevice(_lcd, 0x0008, 0x000B);
 
             _cpu = new Z80(_bus, _interruptManager);
 
-            _timer = new System.Timers.Timer(1000 / _framesPerSecond);
-
-            _timer = new System.Timers.Timer(1000 / _framesPerSecond);
         }
 
         public void Reset()
         {
+            _rom.Reset();
+            _ram.Reset();
             _cpu.Reset();
             _ctc.Reset();
             _pio.Reset();
             _lcd.Reset();
         }
 
-        public void Start()
-        {
-            _timer.Elapsed += OnTimedEvent;
-            _timer.AutoReset = true;
-            _timer.Enabled = true;
-        }
-
-        public void Stop()
-        {
-            _timer.Stop();
-        }
-
-        public void SetSpeed(int speed)
-        {
-            if (speed < 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(speed), "Speed must be greater than 0.");
-            }
-            _framesPerSecond = speed;
-            _instructionsPerFrame = _instructionsPerSecond / _framesPerSecond;
-            _timer.Interval = 1000 / _framesPerSecond;
-        }
-
-        public void SetSingleStep(bool singleStep)
-        {
-            _singleStep = singleStep;
-        }
-
-        public void LoadROM(byte[] data)
-        {
-            if (data.Length > _rom.Size)
-            {
-                throw new ArgumentException("ROM size exceeds memory size.");
-            }
-            _rom.LoadState(data);
-        }
-
-        public byte[] SaveState()
-        {
-            List<byte> state = new List<byte>();
-            state.AddRange(_cpu.SaveState());
-            state.AddRange(_ctc.SaveState());
-            state.AddRange(_pio.SaveState());
-            state.AddRange(_lcd.SaveState());
-            return state.ToArray();
-        }
-
-        public void LoadState(byte[] state)
-        {
-            int offset = 0;
-            _cpu.LoadState(state.Skip(offset).Take(_cpu.SaveState().Length).ToArray());
-            offset += _cpu.SaveState().Length;
-            _ctc.LoadState(state.Skip(offset).Take(_ctc.SaveState().Length).ToArray());
-            offset += _ctc.SaveState().Length;
-            _pio.LoadState(state.Skip(offset).Take(_pio.SaveState().Length).ToArray());
-            offset += _pio.SaveState().Length;
-            _lcd.LoadState(state.Skip(offset).Take(_lcd.SaveState().Length).ToArray());
-        }
 
         private void Tick(int cycles)
         {
             int cyclesElapsed = 0;
             while(cyclesElapsed < cycles)
             {
-                int instructionCycles = _cpu.Tick();
-                for (int i = 0; i < instructionCycles; i++)
+                int currentCycles = _cpu.Tick();
+                for (int i = 0; i < currentCycles; i++)
                 {
                     _ctc.Tick();
                     _pio.Tick();
                     _lcd.Tick();
                 }
-                cyclesElapsed += instructionCycles;
+                cyclesElapsed += currentCycles;
             }
         }
 
-        private void OnTimedEvent(Object? source, System.Timers.ElapsedEventArgs e)
+        private void RunFrame()
         {
-            if (_singleStep)
+            _stopwatch.Restart();
+            // TODO: Process input
+            Tick(_instructionsPerFrame);
+            // TODO: Update display
+            _stopwatch.Stop();
+            // Wait for the next frame
+            long elapsedMilliseconds = _stopwatch.ElapsedMilliseconds;
+            if (elapsedMilliseconds < _secondsPerFrame * 1000)
             {
-                Tick(1);
-            }
-            else
-            {
-                Tick(_instructionsPerFrame);
+                Thread.Sleep((int)(_secondsPerFrame * 1000) - (int)elapsedMilliseconds);
             }
         }
 
-
-        private System.Timers.Timer _timer;
         private Z80 _cpu;
         private MemoryBus _bus;
         private InterruptManager _interruptManager;
@@ -138,9 +147,13 @@ namespace Ceres80Emu.Emulator
         private PIO _pio;
         private LCD _lcd;
 
+        private Stopwatch _stopwatch = new Stopwatch();
+
+        private const int _framesPerSecond = 60;
+        private const float _secondsPerFrame = 1f / _framesPerSecond;
         private const int _instructionsPerSecond = 6144000;
-        private int _framesPerSecond = 60;
-        private int _instructionsPerFrame = _instructionsPerSecond / 60;
-        bool _singleStep = false;
+        private int _instructionsPerFrame = _instructionsPerSecond / _framesPerSecond;
+        private bool _running = false;
+
     }
 }
