c669953dee25d533c20a396ab7fa8caad5baf919
ImpendingMoon <theimpendingmoon@gmail.com>
2025-10-04 21:46:49 -0500

    Add Ceres80 class stub, improve Registers
    - Create RegisterTarget enum
    - Add Get register by enum methods
    -Rename alt registers to match old code
    - Simplify flag properties

---
+++ Emulator/Ceres80.cs
@@ -0,0 +1,50 @@
+﻿namespace Ceres80Emu.Emulator
+{
+    internal class Ceres80
+    {
+        public DecodedInstruction DecodeInstruction(ushort address)
+        {
+            return new();
+        }
+
+        public void Reset()
+        {
+
+        }
+
+        public void Load(Span<byte> data, ushort address)
+        {
+
+        }
+
+        public byte PeekMemory(ushort address)
+        {
+            return 0;
+        }
+
+        public void PokeMemory(ushort address, byte value)
+        {
+
+        }
+
+        public byte PeekPort(byte address)
+        {
+            return 0;
+        }
+
+        public void PokePort(byte address, byte value)
+        {
+
+        }
+
+        public ushort PeekRegister(Registers.RegisterTarget register)
+        {
+            return 0;
+        }
+
+        public void PokeRegister(Registers.RegisterTarget register)
+        {
+
+        }
+    }
+}
--- Emulator/Registers.cs
+++ Emulator/Registers.cs
@@ -1,92 +1,158 @@
 ﻿using System.Runtime.InteropServices;
 
-namespace Ceres80Emu.Emulator
+namespace Ceres80Emu.Emulator;
+
+// Does not work on Big Endian devices.
+[StructLayout(LayoutKind.Explicit)]
+internal class Registers
 {
-    // Does not work on Big Endian devices.
-    [StructLayout(LayoutKind.Explicit)]
-    internal struct Registers
+    // Primary Registers
+    [FieldOffset(0)] public ushort AF;
+    [FieldOffset(1)] public byte A;
+    [FieldOffset(0)] public byte F;
+
+    [FieldOffset(2)] public ushort BC;
+    [FieldOffset(3)] public byte B;
+    [FieldOffset(2)] public byte C;
+
+    [FieldOffset(4)] public ushort DE;
+    [FieldOffset(5)] public byte D;
+    [FieldOffset(4)] public byte E;
+
+    [FieldOffset(6)] public ushort HL;
+    [FieldOffset(7)] public byte H;
+    [FieldOffset(6)] public byte L;
+
+    // Alternate Registers
+    [FieldOffset(8)] public ushort AltAF;
+    [FieldOffset(9)] public byte AltA;
+    [FieldOffset(8)] public byte AltF;
+
+    [FieldOffset(10)] public ushort AltBC;
+    [FieldOffset(11)] public byte AltB;
+    [FieldOffset(10)] public byte AltC;
+
+    [FieldOffset(12)] public ushort AltDE;
+    [FieldOffset(13)] public byte AltD;
+    [FieldOffset(12)] public byte AltE;
+
+    [FieldOffset(14)] public ushort AltHL;
+    [FieldOffset(15)] public byte AltH;
+    [FieldOffset(14)] public byte AltL;
+
+    // Other 16-bit Registers
+    [FieldOffset(16)] public ushort IX;
+    [FieldOffset(18)] public ushort IY;
+    [FieldOffset(20)] public ushort SP;
+    [FieldOffset(22)] public ushort PC;
+
+    // Special Registers
+    [FieldOffset(24)] public byte I;
+    [FieldOffset(25)] public byte R;
+    [FieldOffset(26)] public bool IFF1;
+    [FieldOffset(27)] public bool IFF2;
+
+    // Flags
+    public bool Carry
     {
-        // Primary Registers
-        [FieldOffset(0)] public ushort AF;
-        [FieldOffset(1)] public byte A;
-        [FieldOffset(0)] public byte F;
+        get => (F & 0x01) != 0;
+        set => F = (byte)((F & ~0x01) | (value ? 0x01 : 0x00));
+    }
 
-        [FieldOffset(2)] public ushort BC;
-        [FieldOffset(3)] public byte B;
-        [FieldOffset(2)] public byte C;
+    public bool Subtract
+    {
+        get => (F & 0x02) != 0;
+        set => F = (byte)((F & ~0x02) | (value ? 0x02 : 0x00));
+    }
 
-        [FieldOffset(4)] public ushort DE;
-        [FieldOffset(5)] public byte D;
-        [FieldOffset(4)] public byte E;
+    public bool Parity
+    {
+        get => (F & 0x04) != 0;
+        set => F = (byte)((F & ~0x04) | (value ? 0x04 : 0x00));
+    }
 
-        [FieldOffset(6)] public ushort HL;
-        [FieldOffset(7)] public byte H;
-        [FieldOffset(6)] public byte L;
+    public bool HalfCarry
+    {
+        get => (F & 0x10) != 0;
+        set => F = (byte)((F & ~0x10) | (value ? 0x10 : 0x00));
+    }
 
-        // Alternate Registers
-        [FieldOffset(8)] public ushort AF_alt;
-        [FieldOffset(9)] public byte A_alt;
-        [FieldOffset(8)] public byte F_alt;
+    public bool Zero
+    {
+        get => (F & 0x40) != 0;
+        set => F = (byte)((F & ~0x40) | (value ? 0x40 : 0x00));
+    }
 
-        [FieldOffset(10)] public ushort BC_alt;
-        [FieldOffset(11)] public byte B_alt;
-        [FieldOffset(10)] public byte C_alt;
+    public bool Sign
+    {
+        get => (F & 0x80) != 0;
+        set => F = (byte)((F & ~0x80) | (value ? 0x80 : 0x00));
+    }
 
-        [FieldOffset(12)] public ushort DE_alt;
-        [FieldOffset(13)] public byte D_alt;
-        [FieldOffset(12)] public byte E_alt;
+    public enum RegisterTarget
+    {
+        A, F, B, C, D, E, H, L,
+        AltA, AltF, AltB, AltC, AltD, AltE, AltH, AltL,
+        I, R,
+        AF, BC, DE, HL,
+        AltAF, AltBC, AltDE, AltHL,
+        IX, IY, PC, SP,
+        IFF1, IFF2,
+    }
 
-        [FieldOffset(14)] public ushort HL_alt;
-        [FieldOffset(15)] public byte H_alt;
-        [FieldOffset(14)] public byte L_alt;
-
-        // Other 16-bit Registers
-        [FieldOffset(16)] public ushort IX;
-        [FieldOffset(18)] public ushort IY;
-        [FieldOffset(20)] public ushort SP;
-        [FieldOffset(22)] public ushort PC;
-
-        // Special Registers
-        [FieldOffset(24)] public byte I;
-        [FieldOffset(25)] public byte R;
-        [FieldOffset(26)] public bool IFF1;
-        [FieldOffset(27)] public bool IFF2;
-
-        // Flag Properties
-        public bool Carry
+    public ref byte GetByteRegisterRef(RegisterTarget target)
+    {
+        switch (target)
         {
-            get => (F & (1 << 0)) == 1;
-            set => F = (byte)((F & ~(1 << 0)) | (Convert.ToByte(value) << 0));
+        case RegisterTarget.A: return ref A;
+        case RegisterTarget.F: return ref F;
+        case RegisterTarget.B: return ref B;
+        case RegisterTarget.C: return ref C;
+        case RegisterTarget.D: return ref D;
+        case RegisterTarget.E: return ref E;
+        case RegisterTarget.H: return ref H;
+        case RegisterTarget.L: return ref L;
+        case RegisterTarget.AltA: return ref AltA;
+        case RegisterTarget.AltF: return ref AltF;
+        case RegisterTarget.AltB: return ref AltB;
+        case RegisterTarget.AltC: return ref AltC;
+        case RegisterTarget.AltD: return ref AltD;
+        case RegisterTarget.AltE: return ref AltE;
+        case RegisterTarget.AltH: return ref AltH;
+        case RegisterTarget.AltL: return ref AltL;
+        case RegisterTarget.I: return ref I;
+        case RegisterTarget.R: return ref R;
+        default: throw new ArgumentException($"Target {target} is not a byte register.");
         }
+    }
 
-        public bool Subtract
+    public ref ushort GetUShortRegisterRef(RegisterTarget target)
+    {
+        switch (target)
         {
-            get => (F & (1 << 1)) == 1;
-            set => F = (byte)((F & ~(1 << 1)) | (Convert.ToByte(value) << 1));
+        case RegisterTarget.AF: return ref AF;
+        case RegisterTarget.BC: return ref BC;
+        case RegisterTarget.DE: return ref DE;
+        case RegisterTarget.HL: return ref HL;
+        case RegisterTarget.AltAF: return ref AltAF;
+        case RegisterTarget.AltBC: return ref AltBC;
+        case RegisterTarget.AltDE: return ref AltDE;
+        case RegisterTarget.AltHL: return ref AltHL;
+        case RegisterTarget.IX: return ref IX;
+        case RegisterTarget.IY: return ref IY;
+        case RegisterTarget.SP: return ref SP;
+        case RegisterTarget.PC: return ref PC;
+        default: throw new ArgumentException($"Target {target} is not a ushort register.");
         }
+    }
 
-        public bool Parity
+    public ref bool GetBoolRegisterRef(RegisterTarget target)
+    {
+        switch (target)
         {
-            get => (F & (1 << 2)) == 1;
-            set => F = (byte)((F & ~(1 << 2)) | (Convert.ToByte(value) << 2));
-        }
-
-        public bool HalfCarry
-        {
-            get => (F & (1 << 4)) == 1;
-            set => F = (byte)((F & ~(1 << 4)) | (Convert.ToByte(value) << 4));
-        }
-
-        public bool Zero
-        {
-            get => (F & (1 << 6)) == 1;
-            set => F = (byte)((F & ~(1 << 6)) | (Convert.ToByte(value) << 6));
-        }
-
-        public bool Sign
-        {
-            get => (F & (1 << 7)) == 1;
-            set => F = (byte)((F & ~(1 << 7)) | (Convert.ToByte(value) << 7));
+        case RegisterTarget.IFF1: return ref IFF1;
+        case RegisterTarget.IFF2: return ref IFF2;
+        default: throw new ArgumentException($"Target {target} is not a bool register.");
         }
     }
 }
